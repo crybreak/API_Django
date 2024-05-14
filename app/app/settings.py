b41d1eddd03c19f674bc4a0d983cea0387dafd68
Wilfried GOSSAN <gossanguy1@gmail.com>
2024-05-14 07:07:00 +0100

    User authentificate manager

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
-
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -131,5 +133,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
-REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'}
-
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,7 +29,7 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    def test_edit_user_page (self):
+    def test_edit_user_page(self):
         """Test that the user edit page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
